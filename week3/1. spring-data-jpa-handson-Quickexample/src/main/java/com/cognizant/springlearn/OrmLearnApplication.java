09cfec1c85d47bd463b616aee7dfd0d9937dc43b
Alekhya <alekhyarayavarapu@gmail.com>
2026-07-09 16:53:10 +0530

    week3 Spring Data JPA Quick Example - Folder Structure

---
+++ week3/1. spring-data-jpa-handson-Quickexample/src/main/java/com/cognizant/springlearn/OrmLearnApplication.java
@@ -0,0 +1,8 @@
+package com.cognizant.springlearn;
+
+public class OrmLearnApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Spring Data JPA Demo (Structure Only)");
+    }
+}
---
+++ week3/1. spring-data-jpa-handson-Quickexample/src/main/java/com/cognizant/springlearn/service/CountryService.java
@@ -0,0 +1,46 @@
+package com.cognizant.springlearn.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cognizant.springlearn.model.Country;
+import com.cognizant.springlearn.repository.CountryRepository;
+import com.cognizant.springlearn.service.exception.CountryNotFoundException;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public Country findCountryByCode(String code) throws CountryNotFoundException {
+        Optional<Country> result = countryRepository.findById(code);
+
+        if (!result.isPresent()) {
+            throw new CountryNotFoundException();
+        }
+
+        return result.get();
+    }
+
+    @Transactional
+    public void addCountry(Country country) {
+        countryRepository.save(country);
+    }
+
+    @Transactional
+    public void updateCountry(String code, String name) {
+        Country country = countryRepository.findById(code).get();
+        country.setName(name);
+        countryRepository.save(country);
+    }
+
+    @Transactional
+    public void deleteCountry(String code) {
+        countryRepository.deleteById(code);
+    }
+}
